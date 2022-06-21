be6eb11e8eb8c06eb4b388517f895feba2ec2636
tal-bendet <tal.bendet@amdocs.com>
2022-06-21 09:23:07 +0300

    change tile css logic

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,13 +3,21 @@ import classes from "./Tile.module.css";
 // todo handle words grouping
 
 const Tile = ({ char, handleClick }) => {
-  const cla =
-    char === " " ? `${classes.tile} ${classes.invisible}` : classes.tile;
   return (
-    <p className={cla} onClick={handleClick}>
+    <p className={classes.tile} onClick={handleClick}>
       {char ? char : "_"}
     </p>
   );
 };
 
 export default Tile;
+
+// const Tile = ({ char, handleClick }) => {
+//   const cla =
+//     char === " " ? `${classes.tile} ${classes.invisible}` : classes.tile;
+//   return (
+//     <p className={cla} onClick={handleClick}>
+//       {char ? char : "_"}
+//     </p>
+//   );
+// };
