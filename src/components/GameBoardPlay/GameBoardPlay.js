be494d52f617690653b9d105341fb4678300c72d
tal-bendet <tal.bendet@amdocs.com>
2022-06-22 22:14:19 +0300

    move some more of the game logic to context

--- src/components/GameBoardPlay/GameBoardPlay.js
+++ src/components/GameBoardPlay/GameBoardPlay.js
@@ -1,62 +1,36 @@
-import { Fragment, useState, useEffect, useContext } from "react";
+import { Fragment, useContext, useState } from "react";
 import GamePlayContext from "../../store/Game-play-context";
 import Card from "../../ui/Card";
 import Tile from "../Tile";
 import PhraseDisplay from "../PhraseDisplay";
-import { LETTERS, MAX_STRIKES } from "./index";
+import { LETTERS } from "./index";
 
 const GameBoardPlay = ({ category }) => {
-  const { secretWord } = useContext(GamePlayContext);
-  const [matchedLetters, setMatchedLetters] = useState([]);
+  const ctx = useContext(GamePlayContext);
   const [lettersBox, setLettersBox] = useState(LETTERS);
   const [usedLettersBox, setUsedLettersBox] = useState([]);
-  const [strikes, setStrikes] = useState(0);
-  const secretWordSet = new Set([...secretWord]);
-  useEffect(() => {
-    if (strikes > MAX_STRIKES) {
-      console.log("YOU LOSERRRRRRRRRRRRRRRRRR");
-    }
-  }, [strikes]);
-  // ! consider letter casing,snake casing or any other casing in the original word,secret word should be trimmed
-  // ! consider special charecters inside a word ('....)
-  const handleTileClick = (tileChar) => {
+  const onTileClick = (tileChar) => {
     const i = lettersBox.indexOf(tileChar);
-    console.log(i);
     const newLettersBox = [...lettersBox];
-    console.log(newLettersBox);
     newLettersBox.splice(i, 1);
-    console.log(newLettersBox);
-    console.log(matchedLetters);
-
     setLettersBox(newLettersBox);
     setUsedLettersBox([...usedLettersBox, tileChar]);
     const lowerTileChar = tileChar.toLowerCase();
-    console.log(lowerTileChar);
-    console.log(secretWordSet);
-    if (secretWordSet.has(lowerTileChar)) {
-      setMatchedLetters([...matchedLetters, lowerTileChar]);
-      // ? some kind of counter?,animation? who knows...
-    } else {
-      setStrikes((prev) => prev + 1);
-      // ? animation
-    }
-    // todo check if the whole word is discovered and then the game is won)
-    // * effect
+    ctx.checkChar(lowerTileChar);
   };
-  // todo move reusable states to context
-  // todo
+
   return (
     <Fragment>
       <Card>
         <h2>{category}</h2>
       </Card>
-      <Card>The answer is...{secretWord}</Card>
-      <PhraseDisplay matchedLetters={matchedLetters}></PhraseDisplay>
+      <Card>The answer is...{ctx.secretWord}</Card>
+      <PhraseDisplay></PhraseDisplay>
       <Card>
         {lettersBox.map((char) => (
           <Tile
             char={char}
-            handleClick={handleTileClick}
+            handleClick={onTileClick}
             key={`lettersBox-${char}`}
           />
         ))}
